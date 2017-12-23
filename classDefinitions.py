1ad2b6c1adeee42e6ee9f3b96aa2f4ccd6a1b3c5
Siddhesh <siddhesh166@gmail.com>
2017-12-22 18:58:24 -0800

    Added class definitions

---
+++ classDefinitions.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import numpy as np
+
+class Node(object):
+	def __init__(self, id, inEdges, outEdges):
+		self.id = id
+		self.inEdges = inEdges
+		self.outEdges = outEdges
+
+class Edge(object):
+	def __init__(self, id, node1, node2, probabilities):
+		self.id = id
+		self.node1 = node1
+		self.node2 = node2
+		self.probabilities = probabilities
+		self.estimates = np.random.rand(len(self.probabilities))
+
+class Graph(object):
+	def __init__(self, nodes, edges):
+		self.nodes = nodes
+		self.edges = edges
+
+
+class Item(object):
+	def __init__(self, id, topicDistribution):
+		self.id = id
+		self.topicDistribution = topicDistribution
+		self.topicEstimates = np.random.rand(len(self.topicDistribution))
+		self.topicEstimates = self.topicEstimates/sum(self.topicEstimates)
+
+class CascadeLog(object):
+	def __init__(self, node1, node2, edge, time):
+		self.node1 = node1
+		self.node2 = node2
+		self.edge = edge
+		self.time = time 
+
+class NodeCascade(object):
+	def _init__(self, node, time):
+		self.node = node
+		self.time = time
