3513b6f31e749ffd9b0fd73a855c5bc4673d697e
Gursimar <simar.i3r@gmail.com>
2018-01-01 21:50:13 -0800

    added baselines

---
+++ Results/plot_graph.py
@@ -0,0 +1,226 @@
+import matplotlib.pyplot as plt
+
+randoml1 = []
+randomregret = []
+highdegl1 = []
+highdegregret = []
+
+def read_baselines():
+	global randoml1
+	global randomregret
+	global highdegl1
+	global highdegregret
+	file1='nethept_highdegree_50seeds_1000iter'
+	file2='nethept_random_50seeds_1000iter'
+	strdata1 = open(file1, 'rb').read().split('\n')
+	strdata2 = open(file2, 'rb').read().split('\n')
+	size = len(strdata1)
+
+	l11 = []
+	l12 = []
+	idl1 = 0
+	regret1 = [] 
+	regret2 = []
+	idregret = 1
+	iterations = []
+	for i,datum in enumerate(strdata1):
+		if (i%2==0) and (i%3==0) and (i%4==0) and (i%6==0) and (i%8==0) and (i%12==0) and (i%16==0):
+			try:
+				l11.append(float(strdata1[i].split(',')[idl1]))
+				l12.append(float(strdata2[i].split(',')[idl1]))
+				regret1.append(float(strdata1[i].split(',')[idregret]))
+				regret2.append(float(strdata2[i].split(',')[idregret]))
+			except:
+				print 'could not parse this -> ' + strdata1[i] + strdata2[i]
+
+	print len(l11)
+	print len(l12)
+	print len(regret1)
+	print len(regret2)
+	randoml1 = l12
+	randomregret = regret2
+	highdegl1 = l11
+	highdegregret = regret1
+
+
+
+def plot_curves2(file1,file2,file3,name):
+	global randoml1
+	global randomregret
+	global highdegl1
+	global highdegregret
+	strdata1 = open(file1, 'rb').read().split('\n')
+	strdata2 = open(file2, 'rb').read().split('\n')
+	strdata3 = open(file3, 'rb').read().split('\n')
+	size = len(strdata1)
+
+	l11 = []
+	l12 = []
+	l13 = []
+	idl1 = 1
+	regret1 = [] 
+	regret2 = []
+	regret3 = []
+	idregret = 2
+	iterations = []
+	for i,datum in enumerate(strdata1):
+		if (i%2==0) and (i%3==0) and (i%4==0) and (i%6==0) and (i%8==0) and (i%12==0) and (i%16==0):
+			try:
+				l11.append(float(strdata1[i].split(' ')[idl1]))
+				l12.append(float(strdata2[i].split(' ')[idl1]))
+				l13.append(float(strdata3[i].split(' ')[idl1]))
+				regret1.append(float(strdata1[i].split(' ')[idregret]))
+				regret2.append(float(strdata2[i].split(' ')[idregret]))
+				regret3.append(float(strdata3[i].split(' ')[idregret]))
+				iterations.append(i+1)
+			except:
+				print 'could not parse this -> ' + strdata1[i] + strdata2[i] + strdata3[i] 
+
+	print len(l11)
+	print len(l12)
+	print len(l13)
+	print len(regret1)
+	print len(regret2)
+	print len(regret3)
+
+	print iterations
+	print l11
+
+	randoml1 = randoml1[0:len(iterations)]
+	highdegl1 = highdegl1[0:len(iterations)]
+	randomregret = randomregret[0:len(iterations)]
+	highdegregret = highdegregret[0:len(iterations)]
+
+	plt.plot(iterations,l11,'o-')
+	plt.plot(iterations,l12,'s-')
+	plt.plot(iterations,l13,'x-')
+	plt.plot(iterations,randoml1,'*-')
+	plt.plot(iterations,highdegl1,'2-')
+	plt.grid(True)
+	plt.legend(['Pure exploration','Epsilon - 0.5','Pure exploitation','Random', 'High Degree'])
+	plt.xlabel('Rounds')
+	plt.ylabel('Relative error')
+	plt.title('Relative error vs rounds')
+	plt.savefig(name + '_l1.png')
+	plt.show()
+
+	plt.figure()
+	plt.plot(iterations,regret1,'o-')
+	plt.plot(iterations,regret2,'s-')
+	plt.plot(iterations,regret3,'x-')
+	plt.plot(iterations,randomregret,'*-')
+	plt.plot(iterations,highdegregret,'2-')
+	plt.grid(True)
+	plt.legend(['Pure exploration','Epsilon - 0.5','Pure exploitation','Random', 'High Degree'])
+	plt.xlabel('Rounds')
+	plt.ylabel('Average regret')
+	plt.title('Average regret vs rounds')
+	plt.savefig(name + '_regret.png')
+	plt.show()
+
+
+def plot_curves(file1,file2,file3,name):
+	global randoml1
+	global randomregret
+	global highdegl1
+	global highdegregret
+	strdata1 = open(file1, 'rb').read().split('\n')
+	strdata2 = open(file2, 'rb').read().split('\n')
+	strdata3 = open(file3, 'rb').read().split('\n')
+	size = len(strdata1)
+
+	l11 = []
+	l12 = []
+	l13 = []
+	idl1 = 0
+	regret1 = [] 
+	regret2 = []
+	regret3 = []
+	idregret = 1
+	iterations = []
+	for i,datum in enumerate(strdata1):
+		if (i%2==0) and (i%3==0) and (i%4==0) and (i%6==0) and (i%8==0) and (i%12==0) and (i%16==0):
+			try:
+				l11.append(float(strdata1[i].split(',')[idl1]))
+				l12.append(float(strdata2[i].split(',')[idl1]))
+				l13.append(float(strdata3[i].split(',')[idl1]))
+				regret1.append(float(strdata1[i].split(',')[idregret]))
+				regret2.append(float(strdata2[i].split(',')[idregret]))
+				regret3.append(float(strdata3[i].split(',')[idregret]))
+				iterations.append(i+1)
+			except:
+				print 'could not parse this -> ' + strdata1[i] + strdata2[i] + strdata3[i] 
+
+	print len(l11)
+	print len(l12)
+	print len(l13)
+	print len(regret1)
+	print len(regret2)
+	print len(regret3)
+	randoml1 = randoml1[0:len(iterations)]
+	highdegl1 = highdegl1[0:len(iterations)]
+	randomregret = randomregret[0:len(iterations)]
+	highdegregret = highdegregret[0:len(iterations)]
+
+
+	plt.plot(iterations,l11,'o-')
+	plt.plot(iterations,l12,'s-')
+	plt.plot(iterations,l13,'x-')
+	plt.plot(iterations,randoml1,'*-')
+	plt.plot(iterations,highdegl1,'2-')
+	plt.grid(True)
+	plt.legend(['Pure exploration','Epsilon - 0.5','Pure exploitation','Random', 'High Degree'])
+	plt.xlabel('Rounds')
+	plt.ylabel('Relative error')
+	plt.title('Relative error vs rounds')
+	plt.savefig(name + '_l1.png')
+	plt.show()
+
+	plt.figure()
+	plt.plot(iterations,regret1,'o-')
+	plt.plot(iterations,regret2,'s-')
+	plt.plot(iterations,regret3,'x-')
+	plt.plot(iterations,randomregret,'*-')
+	plt.plot(iterations,highdegregret,'2-')
+	plt.grid(True)
+	plt.legend(['Pure exploration','Epsilon - 0.5','Pure exploitation','Random', 'High Degree'])
+	plt.xlabel('Rounds')
+	plt.ylabel('Average regret')
+	plt.title('Average regret vs rounds')
+	plt.savefig(name + '_regret.png')
+	plt.show()
+
+
+read_baselines()
+print len(randoml1)
+print len(randomregret)
+print len(highdegl1)
+print len(highdegregret)
+
+
+# Edge level synth
+file1 = 'log_synth_edge_0.txt'
+file2 = 'log_synth_edge_0.5.txt'
+file3 = 'log_synth_edge_1.txt'
+#plot_curves(file1,file2,file3,'synth_edge')
+
+# Node level synth
+file1 = 'log_0.000000synth_5000_l.txt'
+file2 = 'log_0.500000synth_5000_l.txt'
+file3 = 'log_1.000000synth_5000_l.txt'
+#plot_curves2(file1,file2,file3,'synth_node')
+
+# Node level nethept
+file1 = 'log_1.000000graph_ic.inf' # seems like mohit has named the files wrong
+file2 = 'log_0.500000graph_ic.inf' # so I have swapped his explore and exploit
+file3 = 'log_0.000000graph_ic.inf'
+#plot_curves2(file1,file2,file3,'nethept_node')
+
+#Edge level nethelp
+file1 = 'nethept_edge_PExplore_50seeds_1000iter'
+file2 = 'nethept_edge_EGreedy_50seeds_1000iter'
+file3 = 'nethept_edge_PExp_50seeds_1000iter'
+#plot_curves(file1,file2,file3,'nethept_edge')
+
+
+
